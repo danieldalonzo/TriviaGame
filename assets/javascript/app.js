630b971782c27438663a78ff35dbfd1c31d2fc3f
Steve A <steve.adamcsik@gmail.com>
2019-04-08 19:18:18 -0400

    saving

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,13 +28,15 @@ var questionArray = [{
 
 //create timer 
 var card = $("#trivia-form");
+var results = $("#results");
 var timer;
 var gameLogic = {
   correct: 0,
   incorrect: 0,
-  counter: 60,
+  counter: 5,
   countDown: function () {
     gameLogic.counter--;
+    console.log(gameLogic.counter);
     $("#gameTimer").text(gameLogic.counter);
     if (gameLogic.counter === 0) {
       console.log("times up");
@@ -44,53 +46,61 @@ var gameLogic = {
 
   //create start game function
   start: function () {
-    timer = setInterval(gameLogic.countDown, 60000);
-    $("#trivia-form").prepend("<h2>Time Remaining: <span id='gameTimer'>120</span> Seconds</h2>")
-    $("#start").remove();
+    $("#trivia-form").empty();
+/* 
+    // reset game info
+    gameLogic.correct = 0;
+    gameLogic.incorrect = 0;
+    gameLogic.counter = 5; */
+
+    timer = setInterval(gameLogic.countDown, 1000);
+    // $("#trivia-form").prepend("<h2>Time Remaining: <span id='gameTimer'>120</span> Seconds</h2>")
+    $("#start-button").hide();
 
     for (var i = 0; i < questionArray.length; i++) {
       card.append("<h2>" + questionArray[i].question + "</h2>");
       for (var j = 0; j < questionArray[i].choices.length; j++) {
-        card.append("<input type='radio' name='question-" + i +
-          "' value='" + questionArray[i].choices[j] + "''>" + questionArray[i].choices[j]);
+        card.append("<input type='radio' class='mx-1' name='question" + i +
+          "'value='" + questionArray[i].choices[j] + "''>" + questionArray[i].choices[j]);
       }
     }
+    
     card.append("<button id='done'>Done</button>");
   },
 
-  done: function () {
+  done: function (event) {
+    // event.preventDefault();
     var inputs = card.children("input:checked");
+    console.log(inputs);
+    $("#start-button").show();
     for (var i = 0; i < inputs.length; i++) {
       if ($(inputs[i]).val() === questionArray[i].answer) {
-        game.correct++;
+        gameLogic.correct++;
       } else {
-        game.incorrect++;
+        gameLogic.incorrect++;
       }
     }
-    this.result();
+    gameLogic.result();
   },
 
   //create results function
   result: function () {
     clearInterval(timer);
 
-    $("#sub-wrapper h2").remove();
+    $("#trivia-form").remove();
 
-    card.html("<h2>All Done!</h2>");
-    card.append("<h3>Correct Answers: " + this.correct + "</h3>");
-    card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
-    card.append("<h3>UnAnswered: " + (questionArray.length - (this.incorrect + this.correct)) + "</h3>");
+    results.html("<h2>All Done!</h2>");
+    results.append("<h3>Correct Answers: " + this.correct + "</h3>");
+    results.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    results.append("<h3>UnAnswered: " + (questionArray.length - (this.incorrect + this.correct)) + "</h3>");
   }
 };
 
-$("#start").remove();
 
-for (var i = 0; i < questionArray.length; i++) {
-  card.append("<h2>" + questionArray[i].question + "</h2>");
-  for (var j = 0; j < questionArray[i].choices.length; j++) {
-    card.append("<input type='radio' name='question-" + i +
-      "' value='" + questionArray[i].choices[j] + "''>" + questionArray[i].choices[j]);
-  }
-}
+$(document).ready(function() {
+  // bind start button
+  $("#start-button").on("click", gameLogic.start);
 
-card.append("<button id='done'>Done</button>");
+  // bind done button
+  $(document).on("click", "#done", gameLogic.done);
+})
